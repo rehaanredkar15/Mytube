511d13526b2e186106d5a117ff5d73ee153412a2
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-06-01 02:00:27 +0530

    test - made the basic layout and added background dark

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "./components/header/header";
 import Sidebar from "./components/sidebar/Sidebar";
 import { Container } from "react-bootstrap";
+import "./_app.scss";
 import Homescreen from "./screens/Homescreen/Homescreen";
 const App = () => {
   return (
     <>
       <Header />
-      <div className="app_container border border-info">
+      <div className="app__container border border-info">
         <Sidebar />
         <Container fluid className="app_main border border-warning">
           <Homescreen />
