eb9f0f262e0e7b701958603b75c1a06fe15e0302
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-06-02 01:03:07 +0530

    feat - Added the sidebar components and fixed css

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,14 +5,13 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { MdNotifications, MdApps } from "react-icons/md";
 const Header = () => {
   return (
-    <div className="border border-dark">
+    <div className="border border-dark header">
       <FaBars className="header__menu" size={26} />
       <img
-        src="http://pngimg.com/uploads/youtube/youtube_PNG102352.png"
+        src="https://betanews.com/wp-content/uploads/2017/08/new-youtube-logo.jpg"
         alt="Mytube logo"
         className="header__logo"
-      />
-      MYTube
+      />{" "}
       <form>
         <input type="text" placeholder="Search any youtube video" />
         <button type="submit">
@@ -23,7 +22,7 @@ const Header = () => {
         <MdNotifications size={28} />
         <MdApps size={28} />
         <img
-          src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.seekpng.com%2Fpng%2Ffull%2F356-3562377_personal-user.png&imgrefurl=https%3A%2F%2Fwww.seekpng.com%2Fima%2Fu2e6w7a9r5u2y3u2%2F&tbnid=0WCMHJOqQ0-MmM&vet=12ahUKEwiYrfil5_TwAhWfCbcAHQG2BhAQMyhEegQIARB_..i&docid=TI89HUMY6O3bZM&w=356&h=400&q=user%20image&ved=2ahUKEwiYrfil5_TwAhWfCbcAHQG2BhAQMyhEegQIARB_"
+          src="https://www.seekpng.com/png/full/356-3562377_personal-user.png"
           alt="avatar"
         />
       </div>
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,46 @@
 import React from "react";
 import "./_sidebar.scss";
+import {
+  MdSubscriptions,
+  MdExitToApp,
+  MdThumbUp,
+  MdLibraryBooks,
+  MdHome,
+  MdHistory,
+} from "react-icons/md";
+
 const Sidebar = () => {
-  return <div className="border border-danger">this is sidebar</div>;
+  return (
+    <nav className="border border-danger sidebar">
+      <ul>
+        <MdHome size={23} className="sidebar__icons" />
+        <p>Home</p>
+      </ul>
+
+      <ul>
+        <MdThumbUp size={23} className="sidebar__icons" />
+        <p>liked Videos </p>
+      </ul>
+      <ul>
+        <MdSubscriptions size={23} className="sidebar__icons" />
+        <p>Subscriptions </p>
+      </ul>
+      <ul>
+        <MdLibraryBooks size={23} className="sidebar__icons" />
+        <p>Library</p>
+      </ul>
+      <ul>
+        <MdHistory size={23} className="sidebar__icons" />
+        <p>Watch History</p>
+      </ul>
+      <hr />
+      <ul>
+        <MdExitToApp size={23} className="sidebar__icons" />
+        <span>Log Out</span>
+      </ul>
+      <hr />
+    </nav>
+  );
 };
 
 export default Sidebar;
