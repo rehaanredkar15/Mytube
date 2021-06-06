5eb91d8caae3d98b68d0dafd3b0563fdd8f2df24
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-06-06 13:14:40 +0530

    feat - Setting up the router for different pages

--- src/App.js
+++ src/App.js
@@ -6,27 +6,59 @@ import "./_app.scss";
 import Homescreen from "./screens/Homescreen/Homescreen";
 import LoginScreen from "./screens/Loginscreen/Loginscreen";
 
-const Layout = ({children}) => {
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
+
+
+
+//layout component is for displaying whatever props is passed means which ever component is passed 
+const Layout = ({ children }) => {
   const [sidebar, toggleSidebar] = useState(false);
 
   const handleToggle = () => toggleSidebar((value) => !value);
 
   return (
-     <Header handleToggle={handleToggle} />
+    <>
+      <Header handleToggle={handleToggle} />
       <div className="app__container">
         <Sidebar sidebar={sidebar} handleToggle={handleToggle} />
         <Container fluid className="app_main">
-         {children}
+          {children}
         </Container>
-      </div> 
+      </div>
+    </>
   );
 };
 
 const App = () => {
   return (
-    <>
+    <Router>
+      
+      <Switch>
+      <Route  path ='/' exact>
+      <Layout>
+          <Homescreen/>
+      </Layout>
+      </Route>
+
+      <Route path = '/auth'>
+      
       <LoginScreen />
-    </>
+      
+      </Route>
+
+      <Route path = '/search'>
+      <Layout>
+        <h1>Search </h1>
+      </Layout>
+      </Route>
+  
+
+      <Route>
+        <Redirect to ='/' />
+      </Route>
+
+    </Switch>
+    </Router>
   );
 };
 
